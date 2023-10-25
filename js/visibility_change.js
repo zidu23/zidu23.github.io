a951f10aff71266b27222937afbe62070cd18c44
陈平安 <2223953353@qq.com>
2023-10-25 18:05:17 +0800

    Site updated: 2023-10-25 18:05:17

--- js/visibility_change.js
+++ js/visibility_change.js
@@ -2,14 +2,14 @@ var originTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        document.title = '(*σ´∀`)σ 我走噜~ ' + "WLB` Blog";
+        document.title = '(*σ´∀`)σ 我走噜~ ' + "WLB’ Blog";
         if (titleTime != null) {
             clearTimeout(titleTime);
         }
     } else {
-        document.title = '(*´∇｀*) 我来力~ ' + "WLB` Blog";
+        document.title = '(*´∇｀*) 我来力~ ' + "WLB’ Blog";
         titleTime = setTimeout(function () {
-            document.title = "WLB` Blog";
+            document.title = "WLB’ Blog";
         }, 2000);
     }
 });
