a77ba6bd39c314252445dcca00a1c0b30e44411b
陈平安 <2223953353@qq.com>
2023-11-02 10:03:29 +0800

    Site updated: 2023-11-02 10:03:28

--- js/bilibili.js
+++ js/bilibili.js
@@ -1,24 +1,9 @@
-//fetch('https://api.gumengya.com/Api/BiliBliHot?format=json').then(data=>data.json()).then((json)=>{
-//     let html = '<div id="bilibili-container">'
-//     html += '<div class="bilibili-list">'
-//     var i = 1 
-//     for (let item of json.data) {
-//         html += '<div class="bilibili-list-item"><div class="bilibili-hotness">' + i + '</div>' + '<span class="bilibili-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a></span>' + '<div class="bilibili-hot"><span>' + item.hot + '</span></div></div>'
-//         i++
-//     }
-//     html += '</div></div>'
-//     document.getElementById('bilibiliContent').innerHTML = html
-// }).catch(function(error) {
-//     console.log(error);
-// });
-
-// 故梦API失效 暂时替换
-fetch('https://v.api.aa1.cn/api/bilibili-rs/').then(data=>data.json()).then((json)=>{
+fetch('https://api.gumengya.com/Api/BiliBliHot?format=json').then(data=>data.json()).then((json)=>{
     let html = '<div id="bilibili-container">'
     html += '<div class="bilibili-list">'
     var i = 1 
     for (let item of json.data) {
-        html += '<div class="bilibili-list-item"><div class="bilibili-hotness">' + i + '</div>' + '<span class="bilibili-title"><a title="' + item.title + '"href="' + item.link + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a></span>' + '<div class="bilibili-hot"><span>' + item.heat + '</span></div></div>'
+        html += '<div class="bilibili-list-item"><div class="bilibili-hotness">' + i + '</div>' + '<span class="bilibili-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a></span>' + '<div class="bilibili-hot"><span>' + item.hot + '</span></div></div>'
         i++
     }
     html += '</div></div>'
@@ -26,4 +11,19 @@ fetch('https://v.api.aa1.cn/api/bilibili-rs/').then(data=>data.json()).then((jso
 }).catch(function(error) {
     console.log(error);
 });
+
+// 故梦API失效 暂时替换
+// fetch('https://v.api.aa1.cn/api/bilibili-rs/').then(data=>data.json()).then((json)=>{
+//     let html = '<div id="bilibili-container">'
+//     html += '<div class="bilibili-list">'
+//     var i = 1 
+//     for (let item of json.data) {
+//         html += '<div class="bilibili-list-item"><div class="bilibili-hotness">' + i + '</div>' + '<span class="bilibili-title"><a title="' + item.title + '"href="' + item.link + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a></span>' + '<div class="bilibili-hot"><span>' + item.heat + '</span></div></div>'
+//         i++
+//     }
+//     html += '</div></div>'
+//     document.getElementById('bilibiliContent').innerHTML = html
+// }).catch(function(error) {
+//     console.log(error);
+// });
 
